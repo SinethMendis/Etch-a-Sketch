f09f8e720ce81bd05ba958a460f19e2ddf0773ed
Sineth Mendis <mendissineth03@gmail.com>
2023-09-11 17:43:48 +0530

    Created the JavaScript code,Styled the css

--- script.js
+++ script.js
@@ -1,102 +1,55 @@
-const numRows = 16;
-const numColumns = 16;
+const grid = document.querySelector('.grid-panel');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset-btn');
+const applyGridSize = document.querySelector('.apply-btn');
+let squareSize = 8;
 
-const grid = document.querySelector('.grid');
-
-for (let i = 0; i < numRows; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.style.backgroundColor = 'black';
+createGrid(squareSize);
 
 
-    for (let j = 0; j < numColumns; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square)
+// Create Squared Divs
+function createDiv(size) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
 
+    return div;
+}
+
+// Creat The Grid and append it to grid
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid.appendChild(createDiv(grid.clientWidth / gridSize));
+        }
     }
-    grid.appendChild(row);
-
 }
 
 
+function reset() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+    createGrid(squareSize);
+}
 
-
-const body = document.body
-
-const grid1 = document.createElement('div')
-grid1.classList.add('grid1')
-body.appendChild(grid1)
-
-const headerbox = document.createElement('div')
-headerbox.classList.add('headerbox')
-grid1.appendChild(headerbox)
-
-const header = document.createElement('h1')
-header.classList.add('header');
-header.textContent = 'Etch-a-Sketch';
-headerbox.appendChild(header)
-
-const headerbox1 = document.createElement('div')
-headerbox1.classList.add('headerbox1')
-grid1.appendChild(headerbox1)
-
-const headerbox2 = document.createElement('div')
-headerbox2.classList.add('headerbox2')
-grid1.appendChild(headerbox2)
-
-const headerbox3 = document.createElement('div')
-headerbox3.classList.add('headerbox3')
-grid1.appendChild(headerbox3)
-
-
-const squareBoxes = document.querySelectorAll('.square');
-
-
-squareBoxes.forEach(squareBox => {
-    squareBox.addEventListener('mouseenter', () => {
-        changeColor(squareBox, 'yellow');
-    });
-
-
-    squareBox.addEventListener('mouseleave', () => {
-        delayChangeColor(squareBox, 'skyblue', 800);
-    });
-
+// Used event delegation to target children of the grid
+grid.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "box" class
+    if (e.target.matches('.box')) {
+        e.target.classList.add('active');
+    }
 });
 
+gridSize.addEventListener('input', function (e) {
+    squareSize = e.target.value;
+    gridValue.textContent = `${squareSize}x${squareSize}`;
+});
 
-function changeColor(squareBox, color) {
-    squareBox.style.backgroundColor = color;
-}
+applyGridSize.addEventListener('click', function () {
+    reset();
+});
 
-function delayChangeColor(squareBox, color, delay) {
-    setTimeout(() => {
-        changeColor(squareBox, color);
-    }, delay);
-}
-
-
-
-const btn = document.createElement('button')
-btn.classList.add('btn')
-btn.textContent = '16x16'
-headerbox1.appendChild(btn)
-
-const btn1 = document.createElement('button')
-btn1.classList.add('btn1')
-btn1.textContent = 'Custom Size'
-headerbox1.appendChild(btn1)
-
-
-
-
-
-
-
-
-
-
-
-
-
+resetBtn.addEventListener('click', reset);
